77bbcd097d52a7bd89e9f9fe6e9dbf1c10123615
Shan Padayhag <shanpadayhag@gmail.com>
2025-05-24 16:47:02 +0800

    Added login route

--- handler/login/src/main.rs
+++ handler/login/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use ferrum::http::{Response, response::IResponse};
+use lambda_http::{Error, IntoResponse, Request, run, service_fn};
+use serde_json::json;
+
+pub async fn function_handler(_request: Request) -> Result<impl IntoResponse, Error> {
+  let response = Response::new();
+
+  Ok(response.success(&json!({
+    "message": "Hello world!"
+  })))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+  run(service_fn(function_handler)).await
 }
