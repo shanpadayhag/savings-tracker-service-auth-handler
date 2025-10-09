c681ae880c2d403db06acd833704db4027382f37
Shan Padayhag <shanpadayhag@gmail.com>
2025-10-09 09:29:28 +0200

    Added event handler

--- src/main/java/api/savingstracker/authentication_service/functions/Login.java
+++ src/main/java/api/savingstracker/authentication_service/functions/Login.java
@@ -1,16 +1,14 @@
 package api.savingstracker.authentication_service.functions;
 
-import java.util.function.Function;
-
 import org.springframework.stereotype.Component;
-
+import api.savingstracker.authentication_service.annotations.RequestBody;
+import api.savingstracker.authentication_service.aws.ApiGatewayHandler;
 import api.savingstracker.authentication_service.requests.LoginRequest;
 import api.savingstracker.authentication_service.responses.LoginResponse;
 
 @Component
-public class Login implements Function<LoginRequest, LoginResponse> {
-  @Override
-  public LoginResponse apply(LoginRequest request) {
-    return new LoginResponse("dummy-token-for-" + request.email());
-  }
+public class Login extends ApiGatewayHandler {
+    public LoginResponse invoke(@RequestBody LoginRequest request) {
+        return new LoginResponse("dummy-token-for-" + request.email());
+    }
 }
