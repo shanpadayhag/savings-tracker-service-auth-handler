d405a1530308c08008c174824c32ce48af787874
Shan Padayhag <shanpadayhag@gmail.com>
2025-10-17 23:36:07 +0200

    Updated cookie for not strict for same site

--- src/main/java/api/savingstracker/authentication_service/http/CookieService.java
+++ src/main/java/api/savingstracker/authentication_service/http/CookieService.java
@@ -3,7 +3,7 @@ package api.savingstracker.authentication_service.http;
 public class CookieService {
     public String createCookie(String name, String value, long maxAgeInSeconds) {
         return String.format(
-            "%s=%s; Max-Age=%d; Path=/; HttpOnly; Secure; SameSite=Strict",
+            "%s=%s; Max-Age=%d; Path=/; HttpOnly; Secure; SameSite=None",
             name, value, maxAgeInSeconds
         );
     }
