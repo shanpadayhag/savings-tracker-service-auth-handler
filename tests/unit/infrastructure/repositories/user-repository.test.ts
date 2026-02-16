f2623ea4a15f24a59ef7f99926aa0d9cd99f7fcc
Shan Padayhag <shanpadayhag@gmail.com>
2026-02-16 12:23:06 +0100

    Added user repository test for create

--- tests/unit/infrastructure/repositories/user-repository.test.ts
+++ tests/unit/infrastructure/repositories/user-repository.test.ts
@@ -1,13 +1,13 @@
-import { describe, jest, beforeEach } from '@jest/globals';
+import { describe, jest, beforeEach, it, expect } from '@jest/globals';
 import UserRepository from '@/infrastructure/database/repositories/user-repository';
 import * as schema from '@/infrastructure/database/schema';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
-describe('Testing UserRepository', () => {
+describe('UserRepository', () => {
   let repository: UserRepository;
 
-  const mockDBReturning = jest.fn();
-  const mockDBValues = jest.fn(() => ({ returning: mockDBReturning }));
+  const mockDBReturning = jest.fn<() => Promise<schema.User[]>>();
+  const mockDBValues = jest.fn((_values: schema.NewUser) => ({ returning: mockDBReturning }));
   const mockDBInsert = jest.fn(() => ({ values: mockDBValues }));
 
   const mockDB = {
@@ -19,7 +19,35 @@ describe('Testing UserRepository', () => {
     repository = new UserRepository(mockDB);
   });
 
-  describe('', () => {
+  describe('create', () => {
+    it('should create a user and return the created user', async () => {
+      // arrange
+      const newUser = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@example.com',
+        password: 'hashed',
+      };
+      const expectedUser = {
+        id: 'abc-123',
+        firstName: newUser['firstName'],
+        lastName: newUser['lastName'],
+        email: newUser['email'],
+        password: newUser['password'],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      mockDBReturning.mockResolvedValue([expectedUser]);
 
+      // act
+      const createdUser = await repository.create(newUser);
+
+      // assert
+      expect(mockDBInsert).toHaveBeenCalledTimes(1);
+      expect(mockDBValues).toHaveBeenCalledWith(newUser);
+      expect(mockDBValues).toHaveBeenCalledTimes(1);
+      expect(mockDBReturning).toHaveBeenCalledTimes(1);
+      expect(createdUser).toEqual(expectedUser);
+    });
   });
 });
