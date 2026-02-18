4b5e32af7452a5c0d49254f497b7df8263854939
Shan Padayhag <shanpadayhag@gmail.com>
2026-02-18 11:53:11 +0100

    Implemented register user use case

--- src/core/use-cases/register-user.ts
+++ src/core/use-cases/register-user.ts
@@ -1,3 +1,12 @@
-class RegisterUser { }
+import IUserRepository from '@/core/repositories/user-repository';
+import * as schema from '@/infrastructure/database/schema';
+
+class RegisterUser {
+  constructor(private repository: IUserRepository) { }
+
+  async execute(newUser: schema.NewUser) {
+    return this.repository.create(newUser);
+  }
+}
 
 export default RegisterUser;
