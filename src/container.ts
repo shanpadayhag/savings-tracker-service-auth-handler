90b8cb4fdbf6fd57db6955a561e04195a1b99837
Shan Padayhag <shanpadayhag@gmail.com>
2026-02-19 13:48:31 +0100

    Implemented register route

--- src/container.ts
+++ src/container.ts
@@ -1,9 +1,13 @@
+import RegisterUser from '@/core/use-cases/register-user';
 import db from '@/infrastructure/database/drizzle';
 import UserRepository from '@/infrastructure/database/repositories/user-repository';
 import AuthController from '@/presentation/http/controllers/auth-controller';
 
 const userRepository = new UserRepository(db);
-const authController = new AuthController();
+const registerUser = new RegisterUser(userRepository);
+const authController = new AuthController(
+  registerUser,
+);
 
 const container = {
   authController,
--- src/presentation/http/controllers/auth-controller.ts
+++ src/presentation/http/controllers/auth-controller.ts
@@ -1,10 +1,20 @@
+import RegisterUser from '@/core/use-cases/register-user';
 import { Request, Response } from "express";
 
 class AuthController {
-  constructor() { }
+  constructor(
+    private registerUser: RegisterUser,
+  ) { }
 
-  register(_request: Request, respond: Response) {
-    respond.send("This is register route");
+  register = async (request: Request, respond: Response) => {
+    try {
+      const newUser = request.body;
+      const user = await this.registerUser.execute(newUser);
+      respond.status(201).json(user);
+    } catch (error) {
+      console.log(error)
+      respond.status(500).json({ message: 'Internal server error' });
+    }
   }
 }
 
