f6adc1f2964e2f83be744ca9d5473cf810e2bdd7
Shan Padayhag <shanpadayhag@gmail.com>
2026-02-19 13:37:52 +0100

    Updated database config implementation

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,21 +1,13 @@
 import { z } from "zod";
 
 const envDatabase = z.object({
-  DB_HOST: z.string(),
-  DB_PORT: z.coerce.number().default(5432),
-  DB_NAME: z.string(),
-  DB_USER: z.string(),
-  DB_PASSWORD: z.string(),
+  DATABASE_URL: z.string(),
 });
 
 const env = envDatabase.parse(process.env);
 
-const databaseConfig = {
-  host: env.DB_HOST,
-  port: env.DB_PORT,
-  database: env.DB_NAME,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
+const database = {
+  url: env.DATABASE_URL,
 };
 
-export default databaseConfig;
+export default database;
