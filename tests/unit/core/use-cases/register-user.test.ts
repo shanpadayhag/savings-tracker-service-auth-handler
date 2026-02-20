86724915bd01f6197bf69cff93f56b53c2c1cfb7
Shan Padayhag <shanpadayhag@gmail.com>
2026-02-20 14:58:38 +0100

    Updated register user test to test password hashes

--- tests/unit/core/use-cases/register-user.test.ts
+++ tests/unit/core/use-cases/register-user.test.ts
@@ -3,6 +3,7 @@ import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 import * as schema from '@/infrastructure/database/schema';
 import RegisterUser from '@/core/use-cases/register-user';
 import { newUserValid, userValid } from '@/fixtures/user';
+import bcrypt from 'bcryptjs';
 
 const createUserRepository = (overrides: Partial<IUserRepository> = {}): IUserRepository => ({
   create: jest.fn<() => Promise<schema.User>>().mockResolvedValue(userValid),
@@ -26,8 +27,19 @@ describe("RegisterUser", () => {
 
       // assert
       expect(createdUser).toEqual(userValid);
-      expect(repository.create).toHaveBeenCalledWith(newUserValid);
       expect(repository.create).toHaveBeenCalledTimes(1);
+
+      const repositoryCreateCall = (repository.create as jest.Mock).mock.calls[0];
+      expect(repositoryCreateCall).toBeDefined();
+      const repositoryCreateCalledWith = repositoryCreateCall![0] as schema.NewUser;
+
+      expect(repositoryCreateCalledWith.firstName).toBe(newUserValid.firstName);
+      expect(repositoryCreateCalledWith.lastName).toBe(newUserValid.lastName);
+      expect(repositoryCreateCalledWith.email).toBe(newUserValid.email);
+      await expect(bcrypt.compare(
+        newUserValid.password,
+        repositoryCreateCalledWith.password,
+      )).resolves.toBe(true);
     });
   });
 });
