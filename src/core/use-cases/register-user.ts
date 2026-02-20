ac006de630f43725deba951c3b0fa6e7e1a37240
Shan Padayhag <shanpadayhag@gmail.com>
2026-02-20 14:58:55 +0100

    Applied password hashing

--- src/core/use-cases/register-user.ts
+++ src/core/use-cases/register-user.ts
@@ -1,11 +1,14 @@
 import IUserRepository from '@/core/repositories/user-repository';
 import * as schema from '@/infrastructure/database/schema';
+import bcrypt from 'bcryptjs';
 
 class RegisterUser {
   constructor(private repository: IUserRepository) { }
 
   async execute(newUser: schema.NewUser) {
-    return this.repository.create(newUser);
+    return this.repository.create({
+      ...newUser, password: await bcrypt.hash(newUser.password, 12)
+    });
   }
 }
 
