8c24afa99557d60ac1dbfcd34ba2a03606bf0268
Shan Padayhag <shanpadayhag@gmail.com>
2026-02-23 23:46:42 +0100

    Implemented sign token

---
+++ src/shared/utils/jwt.ts
@@ -0,0 +1,27 @@
+import { User } from '@/infrastructure/database/schema';
+import TokenType from '@/shared/enums/token-type';
+import jsonWebToken from '@/config/json-web-token';
+import jwt from 'jsonwebtoken';
+import type { StringValue } from "ms";
+
+type SignTokenPayload = {
+  userID: User['id'];
+};
+
+class Jwt {
+  signToken(payload: SignTokenPayload, tokenType: TokenType) {
+    const tokenTypeIsAccess = tokenType === TokenType.Access;
+    const secret = tokenTypeIsAccess
+      ? jsonWebToken.ACCESS_SECRET
+      : jsonWebToken.REFRESH_SECRET;
+    const expiresIn = tokenTypeIsAccess
+      ? jsonWebToken.ACCESS_EXPIRES_IN
+      : jsonWebToken.REFRESH_EXPIRES_IN;
+
+    return jwt.sign(payload, secret, {
+      expiresIn: expiresIn as StringValue,
+    });
+  }
+}
+
+export default Jwt;
