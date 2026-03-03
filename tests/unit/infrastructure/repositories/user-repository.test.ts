0699aeb30e59e99beb2ea7beca5e12fdf24dbddc
Shan Padayhag <shanpadayhag@gmail.com>
2026-03-03 12:30:11 +0100

    Used reuseable db mock

--- tests/unit/infrastructure/repositories/user-repository.test.ts
+++ tests/unit/infrastructure/repositories/user-repository.test.ts
@@ -1,53 +1,32 @@
+import { newUserValid, userValid } from '@/fixtures/user';
 import { describe, jest, beforeEach, it, expect } from '@jest/globals';
 import UserRepository from '@/infrastructure/database/repositories/user-repository';
-import * as schema from '@/infrastructure/database/schema';
-import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import { createDrizzleMock, drizzleMockFns } from '@/mocks/infrastructure/database/drizzle.mock';
+
+const { insert: { mockDBInsert, mockDBReturning, mockDBValues } } = drizzleMockFns;
 
 describe('UserRepository', () => {
   let repository: UserRepository;
 
-  const mockDBReturning = jest.fn<() => Promise<schema.User[]>>();
-  const mockDBValues = jest.fn((_values: schema.NewUser) => ({ returning: mockDBReturning }));
-  const mockDBInsert = jest.fn(() => ({ values: mockDBValues }));
-
-  const mockDB = {
-    insert: mockDBInsert,
-  } as unknown as PostgresJsDatabase;
-
   beforeEach(() => {
     jest.clearAllMocks();
-    repository = new UserRepository(mockDB);
+    repository = new UserRepository(createDrizzleMock());
   });
 
   describe('create', () => {
     it('should create a user and return the created user', async () => {
       // arrange
-      const newUser = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'john@example.com',
-        password: 'hashed',
-      };
-      const expectedUser = {
-        id: 'abc-123',
-        firstName: newUser['firstName'],
-        lastName: newUser['lastName'],
-        email: newUser['email'],
-        password: newUser['password'],
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-      mockDBReturning.mockResolvedValue([expectedUser]);
+      mockDBReturning.mockResolvedValue([userValid]);
 
       // act
-      const createdUser = await repository.create(newUser);
+      const createdUser = await repository.create(newUserValid);
 
       // assert
       expect(mockDBInsert).toHaveBeenCalledTimes(1);
-      expect(mockDBValues).toHaveBeenCalledWith(newUser);
+      expect(mockDBValues).toHaveBeenCalledWith(newUserValid);
       expect(mockDBValues).toHaveBeenCalledTimes(1);
       expect(mockDBReturning).toHaveBeenCalledTimes(1);
-      expect(createdUser).toEqual(expectedUser);
+      expect(createdUser).toEqual(userValid);
     });
   });
 
